16d3be7df23ea484746d526d4d7feb2be2fb2170
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-16 12:36:03 -0300

    tailwind pallete & font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <>
+    <div className="bg-soft-white">
       <h1 className="bold">HEllo</h1>
-    </>
+    </div>
   );
 };
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "primary-blue": "#004aad",
+      "dark-blue": "#002c74",
+      "medium-blue": "#0066b3",
+      "light-blue": "#80b8e5",
+      "pale-blue": "#cce5f7",
+      "soft-white": "#F2F2F2",
+      "light-gray": "#CCCCCC",
+      "dark-gray": "#666666",
+      "soft-black": "#333333",
+    },
     extend: {},
   },
   plugins: [],
