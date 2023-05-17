f0f81817427a3c9b6e976113acb0b212e9ee3eab
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-17 12:40:14 -0300

    server - createProject mutation

--- server/models/Project.js
+++ server/models/Project.js
@@ -4,8 +4,8 @@ const projectSchema = new Schema(
   {
     name: { type: String, required: true },
     description: { type: String },
-    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    id: { type: Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
+    // createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    // id: { type: Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
   },
   { timestamps: true }
 );
