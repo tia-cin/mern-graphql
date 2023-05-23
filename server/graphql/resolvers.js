ad15179f019c665e16a927103071e75593e3d149
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-23 11:03:36 -0300

    relationships

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -55,4 +55,12 @@ export const resolvers = {
       return updatedTask;
     },
   },
+
+  Project: {
+    tasks: async (parent) => await Task.find({ projectId: parent._id }),
+  },
+
+  Task: {
+    project: async (parent) => await Project.findById(parent.projectId),
+  },
 };
