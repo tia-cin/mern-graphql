7dbf43bdc7150ef1506107ea24dc2a882b730ef9
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-23 11:23:20 -0300

    client - setup navigation fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ProjectDetail, Projects } from "./pages";
 
@@ -13,6 +13,7 @@ const App: React.FC = () => {
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/projects" />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
         </Routes>
