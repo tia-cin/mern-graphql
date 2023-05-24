b0cf2391afc1e3182da21f4ab38745aef38c83b1
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-24 12:42:06 -0300

    server & client - types & fix model

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,9 +1,13 @@
 export interface TaskType {
   _id?: string;
   title: string;
+  description?: string;
   project: ProjectType;
   createdAt?: string;
   updatedAt?: string;
+  status: string;
+  dueDate: string;
+  assignedTo: string;
 }
 
 export interface ProjectType {
@@ -12,5 +16,20 @@ export interface ProjectType {
   description: string;
   createdAt?: string;
   updatedAt?: string;
+  status: string;
   tasks?: TaskType[];
+  owner: UserType;
+  members: UserType[];
+  dueDate: string;
+}
+
+export interface UserType {
+  name: string;
+  password: string;
+  email: string;
+  role: string;
+  projects: ProjectType[];
+  tasks: TaskType[];
+  createdAt?: string;
+  updatedAt?: string;
 }
--- server/models/User.js
+++ server/models/User.js
@@ -4,11 +4,27 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
+    password: { type: String, required: true },
     role: { type: String, requered: true },
     projects: [{ type: Schema.Types.ObjectId, ref: "Project" }],
     tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async (next) => {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default mongoose.model("User", userSchema);
