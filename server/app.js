8426c9ae17b7fffd42fd092ae47a94478a635bb0
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-29 11:58:58 -0300

    client - authentication fixed

--- server/app.js
+++ server/app.js
@@ -14,9 +14,19 @@ export const startServer = async (typeDefs, resolvers) => {
 
   await server.start();
 
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    next();
+  });
+
   app.use(
     "/graphql",
-    cors({ allowedHeaders: "*" }),
+    cors({ allowedHeaders: ["Authorization"] }),
     express.json(),
     expressMiddleware(server)
   );
