3973963bc0c9a5c3916e08e7f387bc9f7143447d
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-29 10:34:44 -0300

    client - add authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ApolloProvider } from "@apollo/client";
+import { Routes, Route } from "react-router-dom";
 import { Landing, ProjectDetail, Projects } from "./pages";
 import { LogIn, Navbar, SignIn } from "./components";
-import client from "./apollo";
 
 const App: React.FC = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <Navbar isAuthenticated={false} />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/login" element={<LogIn />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/projects/:id" element={<ProjectDetail />} />
-          </Routes>
-        </BrowserRouter>
-      </ApolloProvider>
+      <Navbar isAuthenticated={false} />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+      </Routes>
     </div>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./auth.tsx";
+import client from "./apollo.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <AuthProvider>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </ApolloProvider>
+  </AuthProvider>
+);
